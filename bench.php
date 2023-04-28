bbe1f3fb07e7988c5e13345e4c86c66d88f1c0d2
Oleg Kirichenko <50740197+infosave2007@users.noreply.github.com>
2023-04-28 11:49:42 +0300

    Update bench.php

--- bench.php
+++ bench.php
@@ -4,62 +4,92 @@ require_once 'vendor/autoload.php';
 use GraphMatrix5xN\Graph as Graph5xN;
 use DijkstraOptimized\Graph as GraphDijkstra;
 
-function generateRandomGraph($numNodes, $numEdges) {
-    $graph = [];
-    for ($i = 0; $i < $numNodes; $i++) {
-        $nodeName = chr(ord('A') + $i);
-        $graph[$nodeName] = [];
+<? 
+function generateRandomGraph($numNodes, $numEdges)
+{
+    $nodes = [];
+    $edges = [];
+
+    for ($i = 1; $i <= $numNodes; $i++) {
+        $nodes[] = (string)$i;
     }
 
-    $edges = 0;
-    while ($edges < $numEdges) {
-        $source = chr(ord('A') + rand(0, $numNodes - 1));
-        $target = chr(ord('A') + rand(0, $numNodes - 1));
-        $weight = rand(1, 100);
+    for ($i = 0; $i < $numEdges; $i++) {
+        $node1 = $nodes[array_rand($nodes)];
+        $node2 = $nodes[array_rand($nodes)];
 
-        if ($source !== $target && !isset($graph[$source][$target])) {
-            $graph[$source][$target] = $weight;
-            $edges++;
+        while ($node1 == $node2) {
+            $node2 = $nodes[array_rand($nodes)];
         }
+
+        $edges[] = [
+            'node1' => $node1,
+            'node2' => $node2,
+            'weight' => rand(1, 100)
+        ];
     }
 
-    return $graph;
+    return [
+        'nodes' => $nodes,
+        'edges' => $edges
+    ];
 }
 
-$numNodes = 10;
-$numEdges = 20;
+$numNodes = 10000;
+$numEdges = 50000;
+$source = '1';
+$target = $numNodes;
 
 $graphData = generateRandomGraph($numNodes, $numEdges);
 
 $dijkstra = new Dijkstra();
 $graph5xN = new Graph();
 
-foreach ($graphData as $node => $edges) {
+foreach ($graphData['nodes'] as $node) {
     $dijkstra->addNode($node);
-    $graph5xN->addNode($node);
-
-    foreach ($edges as $target => $weight) {
-        $dijkstra->addEdge($node, $target, $weight);
-        $graph5xN->addEdge($node, $target, $weight);
-    }
 }
 
-$source = 'A';
-$target = chr(ord('A') + $numNodes - 1);
+foreach ($graphData['edges'] as $edge) {
+    $dijkstra->addEdge($edge['node1'], $edge['node2'], $edge['weight']);
+}
 
-$startMemoryDijkstra = memory_get_usage();
+$endMemoryDijkstra = memory_get_usage();
+$usedMemoryDijkstra = $endMemoryDijkstra - $startMemoryDijkstra;
+
+
+$endMemoryDijkstra = memory_get_usage();
+$usedMemoryDijkstra = $endMemoryDijkstra - $startMemoryDijkstra;
+
+$startMemory5xN = memory_get_usage();
+
+foreach ($graphData['nodes'] as $node) {
+    $graph5xN->addNode($node);
+}
+
+foreach ($graphData['edges'] as $edge) {
+    $graph5xN->addEdge($edge['node1'], $edge['node2'], $edge['weight']);
+}
+
+$endMemory5xN = memory_get_usage();
+$usedMemory5xN = $endMemory5xN - $startMemory5xN;
+
+
+
+
 $startDijkstra = microtime(true);
 $resultDijkstra = $dijkstra->findShortestPath($source, $target);
+//print_r($resultDijkstra);
 $endDijkstra = microtime(true);
-$endMemoryDijkstra = memory_get_usage();
+
 
 $startMemory5xN = memory_get_usage();
 $start5xN = microtime(true);
 $result5xN = $graph5xN->findShortestPath($source, $target);
+//print_r($result5xN);
 $end5xN = microtime(true);
-$endMemory5xN = memory_get_usage();
 
-echo "Dijkstra: time: " . ($endDijkstra - $startDijkstra) . " sec, memory: " . ($endMemoryDijkstra - $startMemoryDijkstra) . " bytes\n";
-echo "5xN: time: " . ($end5xN - $start5xN) . " sec, memory: " . ($endMemory5xN - $startMemory5xN) . " bytes\n";
+
+echo "Dijkstra: time: " . ($endDijkstra - $startDijkstra) . " sec, memory: " . ($usedMemoryDijkstra) . " bytes\n";
+echo "5xN: time: " . ($end5xN - $start5xN) . " sec, memory: " . ($usedMemory5xN) . " bytes\n";
 
 ?>
